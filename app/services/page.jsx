e19d2ec7a546890a3e4d8efcd13087f900acb534
Ulugbek Kobulov <108155304+KobulovUlugbek@users.noreply.github.com>
2024-07-04 12:37:30 +0200

    Add text outline styles to services section

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,7 +1,85 @@
+"use client";
+
+import { BsArrowDownRight } from "react-icons/bs";
+import Link from "next/link";
+
+const services = [
+  {
+    num: "01",
+    title: "Web Development",
+    description:
+      "We build websites that serve as powerful marketing tools and bring memorable brand experiences.",
+    href: "",
+  },
+  {
+    num: "02",
+    title: "Web Development",
+    description:
+      "We build websites that serve as powerful marketing tools and bring memorable brand experiences.",
+    href: "",
+  },
+  {
+    num: "03",
+    title: "Web Development",
+    description:
+      "We build websites that serve as powerful marketing tools and bring memorable brand experiences.",
+    href: "",
+  },
+  {
+    num: "04",
+    title: "Web Development",
+    description:
+      "We build websites that serve as powerful marketing tools and bring memorable brand experiences.",
+    href: "",
+  },
+];
+
+import { motion } from "framer-motion";
+
 export const Services = () => {
   return (
-    <div>Services page</div>
-  )
-}
+    <section className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0">
+      <div className="container mx-auto">
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{
+            opacity: 1,
+            transition: {
+              duration: 0.4,
+              delay: 2.4,
+              ease: "easeIn",
+            },
+          }}
+          className="grid grid-cols-1 md:grid-cols-2 gap-[60px]"
+        >
+          {services.map((service, index) => {
+            return (
+              <div
+                key={index}
+                className="flex flex-1 flex-col justify-center gap-6 group"
+              >
+                {/* top */}
+                <div className="w-full flex justify-between items-center">
+                  <div className="text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover">
+                    {service.num}
+                  </div>
+                  <Link href={service.href}>
+                    <BsArrowDownRight />
+                  </Link>
+                </div>
+                {/* service title */}
+                <h2>{service.title}</h2>
+                {/* description */}
+                <p>{service.description}</p>
+                {/* border */}
+                <div className="border-b border-white/20 w-full"></div>
+              </div>
+            );
+          })}
+        </motion.div>
+      </div>
+    </section>
+  );
+};
 
 export default Services;
