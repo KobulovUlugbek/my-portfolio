cc60881b9b6ca04854198ba7a8e9af88292b09cf
Ulugbek Kobulov <108155304+KobulovUlugbek@users.noreply.github.com>
2024-07-05 17:58:52 +0200

    Add Swiper component and update dependencies

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -1,7 +1,132 @@
-export const Work = () => {
-  return (
-    <div>Work page</div>
-  )
-}
+"use client";
 
-export default Work
+import { motion } from "framer-motion";
+import React, { useState } from "react";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+
+import { BsArrowUpRight, BsGithub } from "react-icons/bs";
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+import Link from "next/link";
+import Image from "next/image";
+
+const projects = [
+  {
+    num: "01",
+    category: "Web Development",
+    title: "project 1",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    stack: [
+      { name: "Angular" },
+      { name: "TypeScript" },
+      { name: "SCSS" },
+      { name: "Firebase" },
+    ],
+    image: "/",
+    live: "",
+    github: "",
+  },
+  {
+    num: "02",
+    category: "Web Development",
+    title: "project 2",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    stack: [
+      { name: "Angular" },
+      { name: "TypeScript" },
+      { name: "SCSS" },
+      { name: "Firebase" },
+    ],
+    image: "/",
+    live: "",
+    github: "",
+  },
+  {
+    num: "03",
+    category: "Web Development",
+    title: "project 3",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    stack: [
+      { name: "Angular" },
+      { name: "TypeScript" },
+      { name: "SCSS" },
+      { name: "Firebase" },
+    ],
+    image: "/",
+    live: "",
+    github: "",
+  },
+  {
+    num: "04",
+    category: "Web Development",
+    title: "project 4",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    stack: [
+      { name: "Angular" },
+      { name: "TypeScript" },
+      { name: "SCSS" },
+      { name: "Firebase" },
+    ],
+    image: "/",
+    live: "",
+    github: "",
+  },
+];
+
+export const Work = () => {
+  const [project, setProject] = useState(projects[0]);
+
+  return (
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0"
+    >
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row xl:gap-[30px]">
+          <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
+            <div>
+              {/* outline num */}
+              <div className="text-8xl leading-none font-extrabold text-transparent text-outline">
+                {project.num}
+              </div>
+              {/* category */}
+              <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500 capitalize">
+                {project.category} project
+              </h2>
+              {/* project description */}
+              <p className="text-white/60">{project.description}</p>
+              {/* stack */}
+              <ul className="flex gap-4">
+                {project.stack.map((item, index) => {
+                  return (
+                    <li key={index} className="text-xl text-accent">
+                      {item.name}
+                      {index !== project.stack.length - 1 && ","}
+                    </li>
+                  );
+                })}
+              </ul>
+              {/* border */}
+            </div>
+          </div>
+          <div className="w-full xl:w-[50%]">slider</div>
+        </div>
+      </div>
+    </motion.section>
+  );
+};
+
+export default Work;
