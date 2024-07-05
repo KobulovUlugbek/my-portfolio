75709012f65b204e31b7e1249ed0f4f1d172f6f5
Ulugbek Kobulov <108155304+KobulovUlugbek@users.noreply.github.com>
2024-07-05 16:07:41 +0200

    Update email field name in resume page

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -37,7 +37,7 @@ const about = {
       fieldValue: "2+ Years",
     },
     {
-      fieldName: "E-Mail",
+      fieldName: "Email",
       fieldValue: "contact@ulugbek-kobulov.com",
     },
     {
@@ -310,8 +310,34 @@ export const Resume = () => {
             </TabsContent>
 
             {/* about */}
-            <TabsContent value="about" className="w-full">
-              about
+            <TabsContent
+              value="about"
+              className="w-full text-center xl:text-left"
+            >
+              <div className="flex flex-col gap-[30px]">
+                <h3 className="text-4xl font-bold">{about.title}</h3>
+                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+                  {about.description}
+                </p>
+                <ul className="grid grid-cols-1 xl:grid-cols-2 gap-y-6 max-w-[620px] mx-auto xl:mx-0">
+                  {about.info.map((item, index) => {
+                    const noWrapStyle = item.fieldValue.includes("@")
+                      ? { whiteSpace: "nowrap" }
+                      : {};
+                    return (
+                      <li
+                        key={index}
+                        className="flex items-center justify-center xl:justify-start gap-4"
+                      >
+                        <span className="text-white/60">{item.fieldName}</span>
+                        <span className="text-xl" style={noWrapStyle}>
+                          {item.fieldValue}
+                        </span>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
             </TabsContent>
           </div>
         </Tabs>
